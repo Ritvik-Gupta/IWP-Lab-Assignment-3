51e9eb6aba29d4e8322a5b09d2090be843efe0a2
Ritvik Gupta <ritvik.gupta2019@vitstudent.ac.in>
2022-03-11 07:39:34 +0530

    Added Q3 EMI Calculator base setup using Multi Step Form for easy support

---
+++ da-3/Q3 - EMI Calculator/main.js
@@ -0,0 +1,42 @@
+const multiStepForm = document.querySelector("#multi-step-form")
+
+multiStepForm.addEventListener("submit", event => {
+	event.preventDefault()
+	console.log("yeah")
+})
+
+stepForms.forEach(form =>
+	form.addEventListener("keydown", ({ key }) => {
+		if (key === "Enter") {
+			const formButtons = form.querySelectorAll("[data-action]")
+			formButtons[formButtons.length - 1].click()
+		}
+	})
+)
+
+document.querySelectorAll("[data-input-for]").forEach(input =>
+	input.addEventListener("focus", () => {
+		const formControl = input.parentElement
+		formControl.classList.remove("success", "error")
+	})
+)
+
+const nextButtons = [...document.querySelectorAll(`[data-action="next"]`)]
+nextButtons.forEach(button =>
+	button.addEventListener("click", () => {
+		const areValidResponses = validateInputs()
+		if (areValidResponses) performFormSwapAnimations({ updateOf: 1 })
+	})
+)
+
+const previousButtons = [...document.querySelectorAll(`[data-action="previous"]`)]
+previousButtons.forEach(button =>
+	button.addEventListener("click", () => performFormSwapAnimations({ updateOf: -1 }))
+)
+
+const submitButton = document.querySelector(`[data-action="submit"]`)
+submitButton.addEventListener("click", () => {
+	const areValidResponses = validateInputs()
+	if (areValidResponses) {
+	}
+})
